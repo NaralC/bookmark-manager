dc07a66c0609b0791ed2ca7e5f37794311a4fb42
Naral <77269201+NaralC@users.noreply.github.com>
2023-08-27 22:23:31 +0100

    api routes + mocked db

---
+++ server/api/bookmarks/create.post.ts
@@ -0,0 +1,7 @@
+import * as Bookmarks from "@/server/src/bookmark"
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+
+    return Bookmarks.add(body);
+})
---
+++ server/src/bookmark.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { randomUUID } from "node:crypto"
+import { generateIconURL } from "./utils";
+
+type Bookmark = {
+  id: string;
+  url: string;
+  icon: {
+    url: string;
+    version: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const addOptionsSchema = z.object({ url: z.string() });
+type AddOptions = z.infer<typeof addOptionsSchema>;
+
+const bookmarks: Bookmark[] = [];
+
+export async function list(): Promise<Bookmark[]> {
+  return bookmarks;
+}
+
+export async function add(options: AddOptions): Promise<Bookmark> {
+  const params = addOptionsSchema.parse(options);
+
+  const bookmark: Bookmark = {
+    id: randomUUID(),
+    url: params.url,
+    icon: {
+      url: generateIconURL(params.url), 
+      version: 0
+    },
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+
+  bookmarks.push(bookmark);
+  return bookmark;
+}
